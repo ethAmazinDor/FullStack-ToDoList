d7b483abbcc7f67565b47d5b1d999ee273d65df0
Dorian Dubreus <doriandubreus@gmail.com>
2022-08-17 00:29:43 -0400

    added uncheck functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,35 @@
 
-
+const itemCompleted = document.querySelectorAll('.item span.completed')
 const item = document.querySelectorAll('.item span')
 
 Array.from(item).forEach((element) => {
     element.addEventListener('click', markComplete)
 })
 
+
+Array.from(itemCompleted).forEach((element) => {
+    element.addEventListener('click', markUncomplete)
+})
+
+async function markUncomplete() {
+    const itemText = this.parentNode.childNodes[1].innerText
+
+    try {
+        const response = await fetch('markUncomplete', {
+            method: 'put',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                'itemsFromJS': itemText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 
 
